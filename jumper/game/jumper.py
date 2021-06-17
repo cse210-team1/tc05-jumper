869fd8de6e59f8917627456473a0f86541f4b6b7
Jacob Cloward <jacobcloward@yahoo.com>
2021-06-17 16:54:56 -0600

    updated Umper.py

--- jumper/game/jumper.py
+++ jumper/game/jumper.py
@@ -1,7 +1,15 @@
 class Jumper:
     def __init__(self):
-        self.lives = 4
-        self.pic = []
-    def remove_line(self):
-        pass
+        self.lives = 0
+        self.pic = ["  ___  \n", " /___\ \n", " \   / \n", "  \ /  \n",
+                    "   0   \n", "  /|\ \n", "  / \  \n", "\n", "^^^^^^^\n"]
 
+    def remove_line(self):
+        self.pic[self.lives] = "\n"
+        self.lives = self.lives + 1
+
+    def to_string(self):
+        x = self.pic
+        self.pic_string = ""
+        for i in x:
+            self.pic_string += i
