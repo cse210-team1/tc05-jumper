57ba122294af800da53283dbae01437d87707087
Teetothejay13 <teetothejay13@gmail.com>
2021-06-17 19:46:00 -0600

    completed target.py code

--- jumper/game/target.py
+++ jumper/game/target.py
@@ -1,18 +1,78 @@
+from hashlib import new
 import random
+from typing import NewType
 
 class Target:
+    """A code template for a word that is being searched for. The responsibility of this 
+    class of objects is to generate the word that will be searched for, take a character
+    as a guess, and to return a Bool based on whether the guess was within the word. Also,
+    a record of the already-guessed characters is kept track of and updated in both a list
+    of single characters as well as a single string.
+    
+    Stereotype:
+        Searchable item
+
+    Attributes:
+        possible_words (list): A list of possible words that get_rand_word chooses from.
+        word (list): The chosen word, broken up into a list of the individual characters.
+        self.guess (list): A list of correct guesses in the word and underscores representing blank spaces
+        guess (str): A character passed in as a guess.
+        filled_word (str): The self.guess list, condensed into a single string.
+    """
     def __init__(self):
-        self.possible_words = ["Cat", "Bat", "Firetruck", "Playground", "Mouse"]
+        """Initializes all variables.
+
+        Args: 
+            self (Target): An instance of Target.
+            
+        Returns:
+            Nothing.
+        """
+        self.possible_words = ["kayak", "banana", "dog", "firetruck", "teacher", "capatalism"]
         self.word = []   # ex: ['B', 'a', 't']
-        self.guess = []    # ex: ['_', '_', '_']
+        self.guess = []    # ex: ['_', 'a', '_']
+        self.filled_word = ""   # ex: ['_a_']
+
     def get_rand_word(self):
-        number = random.randint(0, len(self.possible_words))
+        """Selects a random word from possible_words, breaks it into characters, and assigns it to word.
+           Also assigns self.guess with the correct amount of underscores.
+
+        Args: 
+            self (Target): An instance of Target.
+            
+        Returns:
+            Nothing.
+        """
+        number = random.randint(0, len(self.possible_words) - 1)
         whole_word = self.possible_words[number]
         self.word = list(whole_word)
-    def check_guess(self):
-        #self.word & self.guess
-        pass
-    def update_guess(self):
-        #self.guess
-        pass
+        for char in self.word:
+            self.guess.append('_')
 
+    def check_guesses(self, guess):
+        """Checks the guess against each letter in word, and if it matches any of them, returns True.
+
+        Args: 
+            self (Target): An instance of Target.
+
+            
+        Returns:
+            Nothing.
+        """
+        guess_is_correct = False
+        guess_letter = str.lower(guess)
+        for char in self.word:
+            if char == guess_letter:
+                guess_is_correct = True
+        return guess_is_correct
+
+    def update_guess(self, guess):
+        word_index = 0
+        whole_word = ""
+        guess_letter = str.lower(guess)
+        for char in self.word:
+            if char == guess_letter:
+                self.guess[word_index] = guess_letter
+            word_index += 1
+        self.filled_word = whole_word.join(self.guess)
+        return self.filled_word
